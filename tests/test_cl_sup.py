a1613b5510be67d9c420f83afc7147b61268306d
Karen Perez Sarmiento kaper@sas.upenn.edu <kaper@gpc2b.sas.upenn.edu>
2023-10-20 18:12:22 -0400

    fixed: cl sup plot

--- tests/test_cl_sup.py
+++ tests/test_cl_sup.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 
 pkcl = pk.Cl_kk_supp()
 pkcl.zsrc = 0.4
+pkcl.alphas = np.arange(20)/40
 cents,data_binned,cinv = pkcl.make_mock_data(sup=True)
 cl_arr = np.vstack((cents,data_binned))
 np.savetxt("../mock_data/mock_sup_cl_0.4.txt",cl_arr)
