ed344dab2de4c9752cb6215f133895a4211640ee
Karen Perez Sarmiento kaper@sas.upenn.edu <kaper@gpc2b.sas.upenn.edu>
2023-11-13 17:39:47 -0500

    remade test plots

--- tests/test_clkk_basis_funcs.py
+++ tests/test_clkk_basis_funcs.py
@@ -9,19 +9,20 @@ import matplotlib.pyplot as plt
 alphas = np.array([0.5,0.3,0.1])
 k_bins = np.array([4.5e-5,4.5e-3,1.0e-1,3.0e3])
 
-pkcl1 = pk1.Cl_kk_supp()
+#pkcl1 = pk1.Cl_kk_supp()
 pkcl2 = pk2.Cl_kk_supp()
 
 pkcl2.k_bins = k_bins
 
-pkcl1.alphas = alphas
+#pkcl1.alphas = alphas
 
-Cl_o = pkcl1.get_Cl_kk(sup=False)
-Cl_1 = pkcl1.get_Cl_kk(sup=True)
+#Cl_o = pkcl1.get_Cl_kk(sup=False)
+#Cl_1 = pkcl1.get_Cl_kk(sup=True)
 Cl_2 = pkcl2.get_Cl_kk()
 
 cents,data_binned,cinv = pkcl2.make_mock_data(alphas,k_bins)
 
+"""
 fig = plt.figure()
 plt.loglog(pkcl1.ells,Cl_o,label="not sup, original")
 plt.loglog(pkcl1.ells,Cl_1,label="sup, original")
@@ -30,19 +31,22 @@ plt.loglog(cents,data_binned,label="sup, new, binned")
 #plt.loglog(cents,data_binned,label="sup, original,binned")
 plt.legend()
 plt.savefig("../plots/cl_kk_old_new.png")
-
+"""
 k_bins = np.round(k_bins,decimals = 6)
 
 fig = plt.figure()
-plt.plot(pkcl1.ells,Cl_o,label="not sup, original")
+#plt.plot(pkcl1.ells,Cl_o,label="not sup, original")
 #plt.loglog(pkcl1.ells,Cl_1,label="sup, origianl")
 for i in range(len(alphas)):
     lab = 'k_bin = ['+str(k_bins[i])+" ,"+str(k_bins[i+1])+"]"
-    plt.plot(pkcl2.ells,Cl_2[:,i],label=lab)
+    plt.plot(pkcl2.ells,pkcl2.ells*Cl_2[:,i],label=lab)
 #plt.loglog(cents,data_binned,label="sup, original,binned")
 plt.xscale("log")
+plt.ylabel("$lC{_l}^{\kappa \kappa}$")
+plt.xlabel("l")
 plt.legend()
 plt.savefig("../plots/cl_kk_fid_basis_funcs.png")
+plt.close(fig)
 
 
 fig = plt.figure()
@@ -50,8 +54,10 @@ fig = plt.figure()
 #plt.loglog(pkcl1.ells,Cl_1,label="sup, origianl")
 for i in range(len(alphas)):
     lab = 'k_bin = ['+str(k_bins[i])+" ,"+str(k_bins[i+1])+"]"
-    plt.plot(pkcl2.ells,alphas[i]*Cl_2[:,i],label=lab)
+    plt.plot(pkcl2.ells,alphas[i]*pkcl2.ells*Cl_2[:,i],label=lab)
 #plt.loglog(cents,data_binned,label="sup, original,binned")
+plt.ylabel("$lC{_l}^{\kappa \kappa}$")
+plt.xlabel("l")
 plt.xscale("log")
 plt.legend()
 plt.savefig("../plots/cl_kk_sup_basis_funcs.png")
